7123a0d5a99796c8b823e5415b5386020c9c1dbc
Carolina Ribeiro <89712311+CarolinaRibeiro790@users.noreply.github.com>
2024-05-02 11:00:15 -0300

    .

--- onebithealth/src/components/Form/index.js
+++ onebithealth/src/components/Form/index.js
@@ -1,7 +1,33 @@
-import React from "react";
-import { View, Text, TextInput } from "react-native";
+import React, {useState} from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import ResultImc from "./ResultIMC/";
+
 //conteudo do Form
 export default function Form(){
+
+const [heigth, setHeight] = useState(null);
+const [weight, setWeight] = useState(null);
+const [messageImc, setMessage] = useState("Preencha o peso e altura:");
+const [imc, setImc] = useState(null);
+const [textButton, setTextButton] = useState("Calcular");
+
+function imcCalculator(){
+    return setImc((weight/(heigth*heigth)).toFixed(2));
+}
+function validationImc(){
+    if(weight != null && heigth != null){
+        imcCalculator();
+        setHeight(null);
+        setWeight(null);
+        setMessageImc("Seu IMC é igual: ");
+        setMessageButton("Calcular Novamente");
+        return;
+    }
+    setImc(null);
+    setTextButton("Calcular");
+    setMessageImc("Preencha o peso e altura");
+}
+
     return(
         <View>
             <View>
@@ -9,7 +35,10 @@ export default function Form(){
                 <TextInput placeholder="Ex. 1.75" keyboardType="numeric"></TextInput>
                 <Text>Peso</Text>
                 <TextInput placeholder="Ex. 75.365" keyboardType="numeric"></TextInput>
+
+                <Button title="Calcular IMC" ></Button>
             </View>
+            <ResultImc messageResultImc = {messageImc} resultImc = {imc} />
         </View>
     );
 }
