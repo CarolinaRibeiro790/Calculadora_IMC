74eae834e75e1acd0eb5426aa3c2f8d11e09809e
Carolina Ribeiro <89712311+CarolinaRibeiro790@users.noreply.github.com>
2024-05-02 14:52:46 -0300

    .

--- onebithealth/App.js
+++ onebithealth/App.js
@@ -16,8 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fe0e5e5',
+    paddingTop: 80,
   },
 });
